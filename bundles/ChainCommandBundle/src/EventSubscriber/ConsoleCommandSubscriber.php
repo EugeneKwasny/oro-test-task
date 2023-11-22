f33172fd3d96cb8a6f69e7edddc37d66cdc0a185
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2023-11-22 23:23:19 +0200

    Add ConsoleCommandSubscriber event subscriber

---
+++ bundles/ChainCommandBundle/src/EventSubscriber/ConsoleCommandSubscriber.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OroTestTask\ChainCommandBundle\EventSubscriber;
+
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+
+
+class ConsoleCommandSubscriber
+{
+
+    public function __invoke(ConsoleCommandEvent $event): void
+    {
+
+    }
+}
